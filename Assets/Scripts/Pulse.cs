f235af76a175559d6947efe15d06e26e71ef89a3
disa <disa@OA-S141-12>
2026-01-13 10:14:37 -0500

    Added curves

--- Assets/Scripts/Pulse.cs
+++ Assets/Scripts/Pulse.cs
@@ -1,11 +1,9 @@
 using UnityEngine;
 
-public class LinearInterpretation : MonoBehaviour
+public class Pulse : MonoBehaviour
 {
-    public Transform start;
-    public Transform end;
-    public float t;
     public AnimationCurve curve;
+    public float t;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,12 +15,14 @@ public class LinearInterpretation : MonoBehaviour
     void Update()
     {
         t += Time.deltaTime;
-        if(t>1)
+        if (t > 1)
         {
             t = 0;
         }
 
-        //transform.position = Vector2.Lerp(start.position, end.position, t);
-        transform.position = Vector2.Lerp(start.position, end.position, curve.Evaluate(t));
+        float y = curve.Evaluate(t);
+        transform.localScale = Vector3.one * curve.Evaluate(t);
+
+
     }
 }
