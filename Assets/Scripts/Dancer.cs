c29be7954f75fe3924ba6fd07f4577463d9d5e19
MININT-5TERVH1\micha <disa@sheridancollege.ca>
2026-01-24 18:40:01 -0500

    Attached flip fuction to dancer sprite with a lerp tied to scale

--- Assets/Scripts/Dancer.cs
+++ Assets/Scripts/Dancer.cs
@@ -3,6 +3,11 @@ using UnityEngine.InputSystem;
 
 public class Dancer: MonoBehaviour
 {
+    //vectors to save the flipped scale states
+    Vector2 lookLeft = new Vector2(1, 1);
+    Vector2 lookRight = new Vector2(-1, 1);
+
+
     public Vector2 startPosition = new Vector2(-6, 0);
     public Vector2 endPosition = new Vector2(6, 0);
     public float t = 0;
@@ -18,5 +23,15 @@ public class Dancer: MonoBehaviour
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         t = Mathf.InverseLerp(startPosition.x, endPosition.x, mousePos.x);
         transform.position = Vector2.Lerp(startPosition, endPosition, t);
+
+        //conditional statements that make the dancers flip at the midpoint
+        if (t >= 0.5)
+        {
+            transform.localScale = Vector2.Lerp(lookLeft, lookRight, t);
+        }
+        if (t <= 0.5)
+        {
+            transform.localScale = Vector2.Lerp(lookRight, lookLeft, t);
+        }
     }
 }
