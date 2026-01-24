6b324e124a3193a111a32e4ebb227f7334857ca5
MININT-5TERVH1\micha <disa@sheridancollege.ca>
2026-01-24 03:11:25 -0500

    Float now functions well enough to work on other things

--- Assets/Scripts/Float.cs
+++ Assets/Scripts/Float.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class Float : MonoBehaviour
 {
+    //lets me plug in a sprite for it to stick to
+    public Transform anchor;
+    //gets the value of the top of the screen in world units
+    float top;
+
     public Vector2 firstPosition = new Vector2(0, 0);
     public Vector2 secondPosition = new Vector2(0, 1);
     public Vector2 thirdPosition = new Vector2(0, Screen.height);
@@ -17,7 +22,8 @@ public class Float : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        transform.position = anchor.position;
+        top = Camera.main.ScreenToWorldPoint(new Vector3(0f, Screen.height, 0f)).y;
     }
 
     // Update is called once per frame
@@ -29,6 +35,13 @@ public class Float : MonoBehaviour
             playing = true;
             leg = 0;
             t = 0f;
+
+            firstPosition = anchor ? (Vector2)anchor.position : (Vector2)transform.position;
+
+            secondPosition = firstPosition + new Vector2(2,3);
+
+            thirdPosition = new Vector2(firstPosition.x, top);
+
             transform.position = firstPosition;
         }
 
@@ -37,7 +50,7 @@ public class Float : MonoBehaviour
 
         if (leg == 0)
         {
-            transform.position = Vector2.Lerp(firstPosition, secondPosition, easedT);
+            transform.position = Vector2.Lerp(firstPosition, secondPosition, t);
 
             if (t >= 1f)
             {
@@ -47,14 +60,14 @@ public class Float : MonoBehaviour
         }
         else 
         {
-            transform.position = Vector2.Lerp(secondPosition, thirdPosition, easedT);
+            transform.position = Vector2.Lerp(secondPosition, thirdPosition, t);
 
             
             if (t >= 1f)
             {
                 Vector2 screenPos = Camera.main.WorldToScreenPoint(transform.position);
 
-                if (screenPos.y > Screen.height)
+                if (screenPos.y >= top)
                 {
                     transform.position = firstPosition;
                     playing = false;
